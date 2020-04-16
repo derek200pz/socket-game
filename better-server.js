cba21c3873163790373bcbc268fe8024013eb928
derek200pz <derek200pp@gmail.com>
2020-04-16 17:23:41 -0500

    images for sprites

--- better-server.js
+++ better-server.js
@@ -6,102 +6,110 @@ var verbose = true;
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 var users = 0;
 var sprites = {};
 
 var speed = 5;
-var sprite1 = {
-        "color": "#f00",
-        "id": "foo",
-        "width": 10,
-        "height": 10,
-        "x": 0,
-        "y": 0
-};
 
 //global functions
 
-var spriteFactory = function(num, color, width, height){
-	return {
-        	"color": color,
-        	"id": num,
-        	"width": width,
-        	"height": height,
-        	"x": 0,
-        	"y": 0
-	};
+//create a sprite object.. very basic
+var spriteFactory = function(num, imgstr) {
+    return {
+        "imgstr": imgstr,
+        "id": num,
+        "x": 0,
+        "y": 0
+    };
 }
 
-var updateSprite = function(sprite, keyCode){	
-	if(keyCode == 37){
-                sprite.x-=speed;
-        }
-	if(keyCode == 38){
-                sprite.y-=speed;
-	}
-        if(keyCode == 39){
-                sprite.x+=speed;
-        }
-        if(keyCode == 40){
-                sprite.y+=speed;
-        }
+//change the coordinates of a sprite given a keycode (only works for arrow keys)
+var updateSprite = function(sprite, keyCode) {
+    if (keyCode == 37) {
+        sprite.x -= speed;
+    }
+    if (keyCode == 38) {
+        sprite.y -= speed;
+    }
+    if (keyCode == 39) {
+        sprite.x += speed;
+    }
+    if (keyCode == 40) {
+        sprite.y += speed;
+    }
 }
 
-var randomColor = function(){
-	const hex = function(num){
-		if(num == 10) return "a";
-		if(num == 11) return "b";
-		if(num == 12) return "c";
-		if(num == 13) return "d";
-		if(num == 14) return "e";
-		if(num == 15) return "f";
-		else return num;
-	}
-	return "#" + hex(Math.floor(Math.random()*16)) + hex(Math.floor(Math.random()*16)) + hex(Math.floor(Math.random()*16));
+//generate a random color hex code (of form #XXX)
+var randomColor = function() {
+    const hex = function(num) {
+        if (num == 10) return "a";
+        if (num == 11) return "b";
+        if (num == 12) return "c";
+        if (num == 13) return "d";
+        if (num == 14) return "e";
+        if (num == 15) return "f";
+        else return num;
+    }
+    return "#" + hex(Math.floor(Math.random() * 16)) + hex(Math.floor(Math.random() * 16)) + hex(Math.floor(Math.random() * 16));
 }
 
 
 
 
-//actual server stuff
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/game.html');
+//serve the homepage
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/game.html');
 });
 
-io.on('connection', function(socket){
-	console.log('a user connected');
-	users++;
-	const usernum = users;
-	socket.emit('startstate', {sprites: sprites});
-	sprites[usernum - 1] = spriteFactory(usernum, randomColor(), 10, 10);
-	io.sockets.emit('newsprite', {num: usernum - 1, sprite: sprites[usernum - 1]});
+//when a new user connects
+io.on('connection', function(socket) {
+    if (verbose) console.log('a user connected');
+    const usernum = users;
+    users++;
 
-	socket.on('movekey', function(data) {
-		updateSprite(sprites[usernum - 1], data.keyCode);
-		console.log({num: usernum - 1, sprite: sprites[usernum - 1]});
-		io.sockets.emit('movement',{num: usernum - 1, sprite: sprites[usernum - 1]});
-	});
+    //send the new user the current state of the room
+    socket.emit('startstate', { sprites: sprites });
 
-	socket.on('disconnect', function (){
-		console.log('A user disconnected');
-		io.sockets.emit("deletesprite", {num: usernum - 1});
-		delete sprites[usernum - 1];
-	});
+    //create and broadcase a new sprite for the new user
+    fs.readFile(__dirname + '/img/delmar.png', function(err, buf) {
+        if (err) {
+            console.log("errorrrrrr");
+        }
+        sprites[usernum] = spriteFactory(usernum, buf.toString('base64'));
+        io.sockets.emit('newsprite', { num: usernum, sprite: sprites[usernum] });
+    });
+
+    //when the user moves, move them
+    socket.on('movekey', function(data) {
+        updateSprite(sprites[usernum], data.keyCode);
+        if (verbose) console.log("moved a sprite, usernum = " + usernum);
+        io.sockets.emit('movement', { num: usernum, newcoords: { x: sprites[usernum].x, y: sprites[usernum].y } });
+    });
+
+    //when the user leaves, delete them
+    socket.on('disconnect', function() {
+        if (verbose) console.log('A user disconnected');
+        io.sockets.emit("deletesprite", { num: usernum });
+        delete sprites[usernum];
+    });
 });
 
-app.get( '/*' , function( req, res, next ) {
+//serve any other pages or files that might exist
+app.get('/*', function(req, res, next) {
 
-	//This is the current file they have requested
-	var file = req.params[0];
+    //This is the current file they have requested
+    var file = req.params[0];
 
-	//For debugging, we can track what files are requested.
-	if(verbose) console.log('\t :: Express :: file requested : ' + file);
+    //For debugging, we can track what files are requested
+    if (verbose) console.log('\t :: Express :: file requested : ' + file);
 
-	//Send the requesting client the file.
-	res.sendFile( __dirname + '/' + file );
+    //Send the requesting client the file
+    res.sendFile(__dirname + '/' + file);
 
-}); 
+});
 
-http.listen(8080, function(){
-	console.log('\tlistening on port 8080');
+//ssshhhhh. listen.
+http.listen(8080, function() {
+    console.log('\tlistening on port 8080');
 });
--- game-script.js
+++ game-script.js
@@ -8,89 +8,110 @@ var sprites = [];
 
 //functions
 
-var draw = function(sprite){
-	ctx.beginPath();
-	ctx.fillStyle = sprite.color;
-	ctx.rect(sprite.x, sprite.y, sprite.width, sprite.height);
-	ctx.fill();
+var draw = function(sprite) {
+    /*ctx.beginPath();
+    ctx.fillStyle = sprite.color;
+    ctx.rect(sprite.x, sprite.y, sprite.width, sprite.height);
+    ctx.fill();*/
+    ctx.drawImage(sprite.img, sprite.x, sprite.y, 96, 192);
 }
 
-var clearCanvas = function(){
-	ctx.clearRect(0,0,canv.clientWidth, canv.clientHeight);
+var drawDelmar = function() {
+    var img = new Image();
+    img.src = "img/delmar.png"
+    ctx.drawImage(img, 0, 0, 96, 192);
+    var speechBubble = new SpeechBubble(ctx);
+    speechBubble.text = "I have a penis!"
+    speechBubble.draw();
 }
 
-var refreshCanvas = function(){	
-	clearCanvas();
-	$.each(sprites, function(i,sprite){
-		draw(sprite);
-	});
+var clearCanvas = function() {
+    ctx.clearRect(0, 0, canv.clientWidth, canv.clientHeight);
+}
+
+var refreshCanvas = function() {
+    clearCanvas();
+    $.each(sprites, function(i, sprite) {
+        console.log(sprite);
+        draw(sprite);
+    });
 }
 
 //after the DOM is ready
 
-$( document ).ready(function(){
-	var socket = io.connect();
-	canv = document.getElementById("canvas");
-	ctx = canv.getContext("2d");
+$(document).ready(function() {
+    var socket = io.connect();
+    canv = document.getElementById("canvas");
+    canv.width
+    ctx = canv.getContext("2d");
+    ctx.imageSmoothingEnabled = false;
 
-	//Now we can listen for that event
+    //Now we can listen for that event
 
-	socket.on('connect', function() {
-		//Note that the data is the object we sent from the server, as is. So we can assume its id exists.
-		console.log( 'Connected successfully to the socket.io server. I\'m the client!');
+    socket.on('connect', function() {
+        //Note that the data is the object we sent from the server, as is. So we can assume its id exists.
+        console.log('Connected successfully to the socket.io server. I\'m the client!');
 
-		var keys = {};
-		var moveUp = function(){socket.emit('movekey', {keyCode: 38});}
-		var moveDown = function(){socket.emit('movekey', {keyCode: 40});}
-		var moveRight = function(){socket.emit('movekey', {keyCode: 39});}
-		var moveLeft = function(){socket.emit('movekey', {keyCode: 37});}
-		window.onkeyup = function(e) { keys[e.keyCode] = false; }
-		window.onkeydown = function(e) { keys[e.keyCode] = true; }
+        var keys = {};
+        var moveUp = function() { socket.emit('movekey', { keyCode: 38 }); }
+        var moveDown = function() { socket.emit('movekey', { keyCode: 40 }); }
+        var moveRight = function() { socket.emit('movekey', { keyCode: 39 }); }
+        var moveLeft = function() { socket.emit('movekey', { keyCode: 37 }); }
+        window.onkeyup = function(e) { keys[e.keyCode] = false; }
+        window.onkeydown = function(e) { keys[e.keyCode] = true; }
 
-		$("#up-button").click(function(){
-			moveUp();
-		});
+        $("#up-button").click(function() {
+            moveUp();
+        });
 
-		$("#down-button").click(function(){
-			moveDown();
-		});
+        $("#down-button").click(function() {
+            moveDown();
+        });
 
-		$("#right-button").click(function(){
-			moveRight();
-		});
+        $("#right-button").click(function() {
+            moveRight();
+        });
 
-		$("#left-button").click(function(){
-			moveLeft();
-		});
+        $("#left-button").click(function() {
+            moveLeft();
+        });
 
-		socket.on('startstate', function(data){
-			sprites = data.sprites;
-			refreshCanvas();
+        socket.on('startstate', function(data) {
+            $.each(data.sprites, function() {
+                var img = new Image();
+                img.src = 'data:image/png;base64,' + this.imgstr;
+                this.img = img;
+            });
+            sprites = data.sprites;
+            refreshCanvas();
+        });
 
-		});
+        socket.on('newsprite', function(data) {
+            console.log(data);
+            var img = new Image();
+            img.src = 'data:image/png;base64,' + data.sprite.imgstr;
+            data.sprite.img = img;
+            sprites[data.num] = (data.sprite);
+            console.log(sprites);
+            refreshCanvas();
+        });
 
-		socket.on('newsprite', function(data){
-			console.log(data);
-			sprites[data.num] = (data.sprite);
-			console.log(sprites);
-			refreshCanvas();
-		});
+        $(document).on("keydown", function(evt) {
+            socket.emit('movekey', { keyCode: evt.keyCode });
+        })
 
-		$( document ).on("keydown", function(evt){
-			socket.emit('movekey', {keyCode: evt.keyCode});
-		})
+        socket.on('movement', function(data) {
+            sprites[data.num].x = data.newcoords.x;
+            sprites[data.num].y = data.newcoords.y;
+            refreshCanvas();
+        });
 
-		socket.on('movement', function(data){
-			sprites[data.num] = data.sprite;
-			refreshCanvas();
-		});
-		
-		socket.on('deletesprite', function(data){
-			delete sprites[data.num];
-			refreshCanvas();
+        socket.on('deletesprite', function(data) {
+            delete sprites[data.num];
+            refreshCanvas();
 
-		});
+        });
 
-	});
+    });
 
 });
