b5bf7077314b34d52c2d6419929dd6edbd292a08
derek200pz <derek200pp@gmail.com>
2020-04-17 17:43:39 -0500

    added one slash

--- simple-server.js
+++ simple-server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/'));
 app.get('/', (req, res) => {
     res.sendFile('game.html');
 });
