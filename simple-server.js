0aee0936c55b6844115c572080a719c4a3b4302f
derek200pz <derek200pp@gmail.com>
2020-04-17 18:02:00 -0500

    verbose needed to exist

--- simple-server.js
+++ simple-server.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
+const verbose = true;
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/game.html');
